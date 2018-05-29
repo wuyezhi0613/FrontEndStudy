1f2df05247a57e1e218a9bc817ec41d75c7cea92
谷中仁 <guzr@summit.com.cn>
2018-05-29 17:51:56 +0800

    continue(cesium&websocket）:整合cesium和websocket

--- server.js
+++ server.js
@@ -2,30 +2,30 @@ var WebSocket = require('ws');
 var axios = require('axios');
 var CircularJSON = require('circular-json');
 var wss = new WebSocket.Server({
-    port: 8181
+  port: 8181
 });
 wss.on('connection', function (ws) {
-    console.log('client connected');
-    ws.on('message', function (message) {
-        console.log(message);
-        ws.send(message)
-    });
-    console.log('ws', ws);
-    getDataByInterval(5000, wss);
+  console.log('client connected');
+  ws.on('message', function (message) {
+    // console.log(message);
+    ws.send(message);
+  });
+  console.log('ws', ws);
+  getDataByInterval(5000, wss);
 });
 
 
 function getDataByInterval(interval /*number*/ , wss) {
-    let url = "https://data-live.flightradar24.com/zones/fcgi/feed.js";
-    setInterval(function () {
-        axios.get(url).then(data => {
-            wss.clients.forEach(client => {
-                if(client.readyState === WebSocket.OPEN) {
-                    client.send(CircularJSON.stringify(data.data));
-                }
-            });
-        }, err => {
-            console.log(err);
-        });
-    }, interval);
+  let url = 'https://data-live.flightradar24.com/zones/fcgi/feed.js';
+  setInterval(function () {
+    axios.get(url).then(data => {
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(CircularJSON.stringify(data.data));
+        }
+      });
+    }, err => {
+      console.log(err);
+    });
+  }, interval);
 }
