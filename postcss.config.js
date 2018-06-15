9e824ce37d515f8088040dd0fc68307585195379
谷中仁 <guzr@summit.com.cn>
2018-06-15 14:05:30 +0800

    :racehorse:chore(postcss)优化css打包过程

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,21 @@
-module.exports = {
+const isProduction = process.argv.indexOf('production') > 0
+
+const config = {
   plugins: [
-    require('autoprefixer')({
-      browsers: [
-        '>5%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9' // React doesn't support IE8 anyway
-      ],
-      flexbox: 'no-2009'
-    })
+    require('postcss-easy-import')({
+      prefix: '_',
+      extensions: ['pcss', 'css']
+    }),
+    require('autoprefixer')(),
+    require('precss')()
   ]
 }
+
+if (isProduction) {
+  config.plugins.push(require('cssnano')({ preset: 'default' }))
+} else {
+  // reduces calc(10px * 2) to 20px
+  config.plugins.push(require('postcss-calc')())
+}
+
+module.exports = config
