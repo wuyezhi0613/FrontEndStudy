3fbba9a9cb0fdb40f0e4f6cd7e882ecb981119df
谷中仁 <guzr@summit.com.cn>
2018-06-16 11:29:06 +0800

    :bug::lipstick::wrench:chore(weboack&config):修改webpack配置文件适合项目打包及对项目名称进行配置化

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
-export default {
+const Config =  {
+  projectName: 'yanqi', // 项目名称， 关系到打包后程序运行的uri
   maptalksMap: ''
 }
+
+export default Config
--- src/routers.tsx
+++ src/routers.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import IllegalElectromechanicalWell from './container/IllegalElectromechanicalWell'
 import App from './components/App'
+import Config from './config'
+
+
 const Index = () => (
-  <Router basename="/">
-    {/* <div style={{ height: "100%" }}> */}
-    {/* 预留菜单栏 */}
-    <Switch>
-      <Route exact path="/" component={IllegalElectromechanicalWellComp} />
-      <Route path="/app" component={App} />
-      <Route component={NoMatch} />
-    </Switch>
-    {/* </div> */}
+  <Router basename={`/${Config.projectName}`}>
+    <div style={{ height: "100%" }}>
+      {/* 预留菜单栏 */}
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/app'>app</Link></li>
+            <li><Link to='/schedule'>Schedule</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <Switch>
+        <Route exact path="/" component={IllegalElectromechanicalWellComp} />
+        <Route path="/app" component={App} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
   </Router>
 )
 
@@ -23,7 +35,7 @@ const IllegalElectromechanicalWellComp = ({ match }) => {
 }
 const NoMatch = ({ match }) => {
   return (
-    <h2 style={{textAlign: 'center'}}>4000000000000000000000.............4...........<br />请选择正确的路由。。。</h2>
+    <h2 style={{ textAlign: 'center' }}>4000000000000000000000.............4...........<br />请选择正确的路由。。。</h2>
   )
 }
 
