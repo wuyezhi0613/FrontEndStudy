dd20e7547b2627652ad815ac9cdf148efd92b97a
guzhongren <guzhongren@live.cn>
2018-06-16 23:06:39 +0800

    :sparkles:feat(导航栏)：使用navlink

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
 import IllegalElectromechanicalWell from './container/IllegalElectromechanicalWell'
 import App from './components/App'
 const Index = () => (
   <Router basename="/">
-    {/* <div style={{ height: "100%" }}> */}
-    {/* 预留菜单栏 */}
-    <Switch>
-      <Route exact path="/" component={IllegalElectromechanicalWellComp} />
-      <Route path="/app" component={App} />
-      <Route component={NoMatch} />
-    </Switch>
-    {/* </div> */}
+    <div style={{ height: "100%" }}>
+      {/* 预留菜单栏 */}
+      <div>
+        <NavLink
+          to="/"
+          activeStyle={{ color: 'green', fontWeight: 'bold' }}
+        >home</NavLink>
+        <NavLink
+          to="/app"
+          activeStyle={{ color: 'red', fontWeight: 'bold' }}
+        >app</NavLink>
+        <NavLink
+          to="/test"
+          activeStyle={{ color: 'blue', fontWeight: 'bold' }}
+        >no match</NavLink>
+      </div>
+      <Switch>
+        <Route exact path="/" component={IllegalElectromechanicalWellComp} />
+        <Route path="/app" component={App} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
   </Router>
 )
 
@@ -23,7 +37,7 @@ const IllegalElectromechanicalWellComp = ({ match }) => {
 }
 const NoMatch = ({ match }) => {
   return (
-    <h2 style={{textAlign: 'center'}}>4000000000000000000000.............4...........<br />请选择正确的路由。。。</h2>
+    <h2 style={{ textAlign: 'center' }}>4000000000000000000000.............4...........<br />请选择正确的路由。。。</h2>
   )
 }
 
